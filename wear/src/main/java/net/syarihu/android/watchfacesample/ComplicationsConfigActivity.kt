35ad8f5deccd6f52b234a56e466b787ab26e9654
syarihu <info@syarihu.net>
2018-02-03 21:24:48 +0900

    add complications config

---
+++ wear/src/main/java/net/syarihu/android/watchfacesample/ComplicationsConfigActivity.kt
@@ -0,0 +1,11 @@
+package net.syarihu.android.watchfacesample
+
+import android.app.Activity
+import android.os.Bundle
+
+class ComplicationsConfigActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complications_config)
+    }
+}
