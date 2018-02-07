98527dc7bf110f00be905028028a4389d6e12ff1
syarihu <info@syarihu.net>
2018-02-07 16:48:31 +0900

    refactor

--- wear/src/main/java/net/syarihu/android/watchfacesample/ComplicationsConfigPreviewView.kt
+++ wear/src/main/java/net/syarihu/android/watchfacesample/ComplicationsConfigPreviewView.kt
@@ -35,28 +35,28 @@ class ComplicationsConfigPreviewView : View {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         calendar.timeInMillis = System.currentTimeMillis()
-        val s_time = DateUtils.formatDateTime(
+        val strTime = DateUtils.formatDateTime(
                 context,
                 calendar.timeInMillis,
                 DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_24HOUR
         )
-        timePaint.getTextBounds(s_time, 0, s_time.length, timeBounds)
+        timePaint.getTextBounds(strTime, 0, strTime.length, timeBounds)
         timePosition.set(
                 canvas.width / 2 - timeBounds.width() / 2,
                 canvas.height / 2
         )
-        val s_date = DateUtils.formatDateTime(
+        val strDate = DateUtils.formatDateTime(
                 context,
                 calendar.timeInMillis,
                 DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_WEEKDAY
         )
-        datePaint.getTextBounds(s_date, 0, s_date.length, dateBounds)
+        datePaint.getTextBounds(strDate, 0, strDate.length, dateBounds)
         datePosition.set(canvas.width / 2 - dateBounds.width() / 2, canvas.height / 2 + timeBounds.height() / 2 + (10 / resources.displayMetrics.density).toInt())
 
         canvas.run {
             drawColor(Color.BLACK)
-            drawText(s_time, timePosition.x.toFloat(), timePosition.y.toFloat(), timePaint)
-            drawText(s_date, datePosition.x.toFloat(), datePosition.y.toFloat(), datePaint)
+            drawText(strTime, timePosition.x.toFloat(), timePosition.y.toFloat(), timePaint)
+            drawText(strDate, datePosition.x.toFloat(), datePosition.y.toFloat(), datePaint)
         }
     }
 }
